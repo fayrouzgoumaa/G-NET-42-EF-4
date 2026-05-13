79bc1899986c1c0b5dc75303797d002a68d3efe0
fayro <fayro@FAYROUZGOUMAA>
2026-05-14 01:51:14 +0300

    Account Customer Model

--- G-NET-42-EF-4/Models/AccountCustomer.cs
+++ G-NET-42-EF-4/Models/AccountCustomer.cs
@@ -8,5 +8,18 @@ namespace G_NET_42_EF_4.Models
 {
     internal class AccountCustomer
     {
+        public int AccountNumber { get; set; }
+
+        public Account Account { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public string OwnershipType { get; set; }
+
+        public DateTime OwnershipStartDate { get; set; }
+
+        public string AccountStatus { get; set; }
     }
 }
