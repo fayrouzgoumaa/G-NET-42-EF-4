9ee2e0a424857e978e3b4f127dc3cba911682682
fayro <fayro@FAYROUZGOUMAA>
2026-05-14 01:48:17 +0300

    Account Model

--- G-NET-42-EF-4/Models/Account.cs
+++ G-NET-42-EF-4/Models/Account.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace G_NET_42_EF_4.Models
 {
     internal class Account
     {
+        public int AccountNumber { get; set; }
+
+        public decimal CurrentBalance { get; set; }
+
+        public string AccountType { get; set; }
+
+        public DateTime OpeningDate { get; set; }
+
+        public int BranchCode { get; set; }
+
+        public Branch Branch { get; set; }
+
+        public ICollection<Transaction> Transactions { get; set; }
+            = new List<Transaction>();
+
+        public ICollection<AccountCustomer> AccountCustomers { get; set; }
+            = new List<AccountCustomer>();
     }
 }
