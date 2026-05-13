6cec22a6625f2ef71f59c2a63a377da18f959e01
fayro <fayro@FAYROUZGOUMAA>
2026-05-14 01:46:09 +0300

    Manager

--- G-NET-42-EF-4/Models/Manager.cs
+++ G-NET-42-EF-4/Models/Manager.cs
@@ -8,5 +8,18 @@ namespace G_NET_42_EF_4.Models
 {
     internal class Manager
     {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime HireDate { get; set; }
+
+        public int BranchCode { get; set; }
+
+        public Branch Branch { get; set; }
     }
 }
