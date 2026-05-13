4816686f2ef4702dc01fb11ccf43d148f8cb28db
fayro <fayro@FAYROUZGOUMAA>
2026-05-13 03:27:44 +0300

    Add project files.

---
+++ G-NET-42-EF-4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_NET_42_EF_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
