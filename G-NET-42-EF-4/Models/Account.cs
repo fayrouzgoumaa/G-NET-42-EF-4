a407812bdf8049f05af9635884613a3a88f05fe8
fayro <fayro@FAYROUZGOUMAA>
2026-05-14 01:45:02 +0300

    Branch model

---
+++ G-NET-42-EF-4/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_42_EF_4.Models
+{
+    internal class Account
+    {
+    }
+}
---
+++ G-NET-42-EF-4/Models/Branch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_42_EF_4.Models
+{
+    internal class Branch
+    {
+        public int Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+       
+        public Manager Manager { get; set; }
+
+        public ICollection<Account> Accounts { get; set; }
+            = new List<Account>();
+    }
+}
