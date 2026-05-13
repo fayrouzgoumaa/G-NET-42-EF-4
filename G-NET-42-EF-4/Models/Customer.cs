982aade430337b614b04b9e6c2862b06394a5762
fayro <fayro@FAYROUZGOUMAA>
2026-05-14 01:47:31 +0300

    Customer Model

--- G-NET-42-EF-4/Models/Customer.cs
+++ G-NET-42-EF-4/Models/Customer.cs
@@ -8,5 +8,23 @@ namespace G_NET_42_EF_4.Models
 {
     internal class Customer
     {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string NationalId { get; set; }
+
+        public string CustomerType { get; set; }
+
+        public ICollection<AccountCustomer> AccountCustomers { get; set; }
+            = new List<AccountCustomer>();
     }
 }
